2706248338124270ffb9b30856721e9298129770
Andrew Tsao <atsaotsao@gmail.com>
2025-08-07 23:32:53 -0700

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,6 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // You can customize your config here
+});
+
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}',
+    './public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/typography')],
+}
